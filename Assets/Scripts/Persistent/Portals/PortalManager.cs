93abe773b41aa06fb9dc79f82aa5c80daa9701f5
Manav <30342951+MzaxnaV@users.noreply.github.com>
2024-02-16 10:40:59 +0000

    add standard assets and modify fpscontroller to work with portals

---
+++ Assets/Scripts/Persistent/Portals/PortalManager.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using Portals.Utility;
+
+namespace Portals
+{
+    /// <summary>
+    /// Used to manage currently active Portals.
+    /// </summary>
+    public class PortalManager : MonoBehaviourSingleton<PortalManager>
+    {
+        #region PrivateFields
+
+        private HashSet<Portal> portals = new HashSet<Portal>();
+
+        #endregion
+    
+        #region PublicProperties
+
+        public IReadOnlyCollection<Portal> Portals => this.portals;
+
+        #endregion
+
+        #region InternalMethods
+
+        /// <summary>
+        /// Called by a portal when it is enabled.
+        /// </summary>
+        /// <param name="portal"></param>
+        internal void Subscribe(Portal portal)
+        {
+            if (!portal.Destination)
+                throw new System.NullReferenceException($"{portal.name} has no destination. Please, set a destination for it.");
+            this.portals.Add(portal);
+        }
+
+        /// <summary>
+        /// Called by a portal when it is disabled.
+        /// </summary>
+        /// <param name="portal"></param>
+        internal void Unsubscribe(Portal portal)
+        {
+            this.portals.Remove(portal);
+        }
+
+        #endregion
+    }
+}
