776442b68981b65795e05b1899855483bd70d4c8
Manav <30342951+MzaxnaV@users.noreply.github.com>
2024-02-17 22:27:43 +0000

    remove assets that give issues in build

--- Assets/Scripts/Utils/LoadSceneTriggerEditor.cs
+++ Assets/Scripts/Utils/LoadSceneTriggerEditor.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 
 namespace Utils
 {
-    [CustomEditor(typeof(LoadSceneTrigger))]
-    public class LoadSceneTriggerEditor : Editor
-    {
-        void OnSceneGUI()
-        {
-            var trigger = (LoadSceneTrigger)target;
-
-            if (trigger.portalExit != null)
-            {
-                Handles.color = Color.black; // Set the color of the line
-
-                Handles.DrawDottedLine(trigger.transform.position, trigger.portalExit.transform.position, 5f);
-            }
-        }
-    }
+    // [CustomEditor(typeof(LoadSceneTrigger))]
+    // public class LoadSceneTriggerEditor : Editor
+    // {
+    //     void OnSceneGUI()
+    //     {
+    //         var trigger = (LoadSceneTrigger)target;
+    //
+    //         if (trigger.portalExit != null)
+    //         {
+    //             Handles.color = Color.black; // Set the color of the line
+    //
+    //             Handles.DrawDottedLine(trigger.transform.position, trigger.portalExit.transform.position, 5f);
+    //         }
+    //     }
+    // }
 }
--- Assets/Scripts/Utils/PortalLabel.cs
+++ Assets/Scripts/Utils/PortalLabel.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 
 namespace Utils
 {
-    [CustomEditor(typeof(Portal))]
-    public class PortalSelection : Editor
-    {
-        private static GUIStyle _labelStyle;
-        private static readonly Vector3 Offset = new (0f, 4f, 0f);
-
-        private void OnEnable()
-        {
-            _labelStyle = new GUIStyle
-            {
-                normal =
-                {
-                    textColor = Color.black
-                },
-                alignment = TextAnchor.MiddleCenter
-            };
-        }
-
-        private void OnSceneGUI()
-        {
-            var portal = (Portal)target;
-            
-            Handles.BeginGUI();
-            Handles.Label(portal.transform.position + Offset, portal.info.GetEditorText(), _labelStyle);
-            Handles.EndGUI();
-        }
-    }
+    // [CustomEditor(typeof(Portal))]
+    // public class PortalSelection : Editor
+    // {
+    //     private static GUIStyle _labelStyle;
+    //     private static readonly Vector3 Offset = new (0f, 4f, 0f);
+    //
+    //     private void OnEnable()
+    //     {
+    //         _labelStyle = new GUIStyle
+    //         {
+    //             normal =
+    //             {
+    //                 textColor = Color.black
+    //             },
+    //             alignment = TextAnchor.MiddleCenter
+    //         };
+    //     }
+    //
+    //     private void OnSceneGUI()
+    //     {
+    //         var portal = (Portal)target;
+    //         
+    //         Handles.BeginGUI();
+    //         Handles.Label(portal.transform.position + Offset, portal.info.GetEditorText(), _labelStyle);
+    //         Handles.EndGUI();
+    //     }
+    // }
 }
