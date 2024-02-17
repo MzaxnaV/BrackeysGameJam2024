2aff756a2eb19a0bfbedbbdc4365cce809107758
Manav <30342951+MzaxnaV@users.noreply.github.com>
2024-02-17 19:24:52 +0000

    mirror

--- Assets/Scripts/MirrorRotation.cs
+++ Assets/Scripts/MirrorRotation.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [RequireComponent((typeof(BoxCollider)))]
@@ -11,6 +12,21 @@ public class MirrorRotation : MonoBehaviour
         if (!boxCollider.isTrigger)
         {
             Debug.LogError("Set " + gameObject.name + "'s collider to Trigger!");
+        }
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.blue);
+        if (Physics.Raycast(transform.position, forward, out hit, Mathf.Infinity)) {
+            if (hit.collider.CompareTag("Door")) {
+                Debug.Log("Mirror looks at shy door!");
+            }
+        } else
+        {
+            Debug.Log("Mirror looks away from shy door!");
         }
     }
 
--- Assets/Scripts/RaycastManager.cs
+++ Assets/Scripts/RaycastManager.cs
@@ -6,18 +6,16 @@ public class RaycastManager : MonoBehaviour
         RaycastHit hit;
         Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
         Debug.DrawRay(transform.position, forward, Color.green);
-
-        if (Physics.Raycast(transform.position, forward, out hit)) {
-            if (hit.collider.CompareTag("Mirror")) {
-                Debug.Log("Looking at mirror");
-                // Calculate reflection
-                Vector3 reflectDir = Vector3.Reflect(forward, hit.normal);
-                Debug.DrawRay(hit.point, reflectDir, Color.blue);
-                // Cast another ray in the reflected direction
-            } else if (!hit.collider.CompareTag("Door")) {
-                // Door is not being looked at directly
-                Debug.Log("Looking at door");
+        
+        // Infinity Raycast
+        if (Physics.Raycast(transform.position, forward, out hit, Mathf.Infinity)) {
+            if (hit.collider.CompareTag("Door")) {
+                Debug.Log("Shy door closed!");
+            }
+            else
+            {
+                Debug.Log("Shy door opens!");
             }
         }
     }
-}
+}   
