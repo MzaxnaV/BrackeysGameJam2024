f49912ac79b382d5bc6d25631760342ca03b497b
Manav <30342951+MzaxnaV@users.noreply.github.com>
2024-02-17 14:36:49 +0000

    Update PortalInfo.cs
    portal script

--- Assets/Scripts/Persistent/Portals/PortalInfo.cs
+++ Assets/Scripts/Persistent/Portals/PortalInfo.cs
@@ -5,9 +5,13 @@ namespace Portals
     [CreateAssetMenu(fileName = "PortalInfoData", menuName = "Portals/PortalInfoData", order = 1)]
     public class PortalInfo : ScriptableObject
     {
+        [Header("Portal")]
         public PortalTag source;
         public PortalTag destination;
-        
+
+        [Header("Skybox")]
+        public Color tint;
+        public Color ground;
         public string GetEditorText() => source + " --> " + destination;
 
     }
