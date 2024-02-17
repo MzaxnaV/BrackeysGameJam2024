1945d4f2739001aedd206982668d8d5ccfb8dd19
CCCCRRRR2000 <160192551+CCCCRRRR2000@users.noreply.github.com>
2024-02-17 16:54:13 +0800

    FOH Basic finished

--- Assets/Scenes/Level0/Scripts/Height.cs
+++ Assets/Scenes/Level0/Scripts/Height.cs
@@ -6,17 +6,26 @@ public class Height : MonoBehaviour
 {
     public float heightChange;
     private Vector3 initialHeight;
+    public GameObject platformTrigger;
+    TriggerOfWeight weightScript;
     // Start is called before the first frame update
     void Start()
     {
         heightChange = 0;
         initialHeight = transform.position;
-        
+
+        weightScript = platformTrigger.GetComponent<TriggerOfWeight>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(initialHeight.x, initialHeight.y + heightChange, initialHeight.z);
+        if (weightScript != null)
+        {
+            heightChange = -weightScript.currentWeight / 10;
+        }
+        
+
     }
 }
---
+++ Assets/Scenes/Level0/Scripts/TriggerOfWeight.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerOfWeight : MonoBehaviour
+{
+    public GameObject triggerPlatform;
+    public float currentWeight;
+    public float targetWeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentWeight = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentWeight = Mathf.Lerp(currentWeight, targetWeight, 1f * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ItemWithWeight"))
+        {
+            targetWeight += 20;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("ItemWithWeight"))
+        {
+            targetWeight -= 20;
+        }
+    }
+}
