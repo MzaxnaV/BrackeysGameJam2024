1ea807a3184bdb1afc84ad9d8d27d121b162d0c0
Manav <30342951+MzaxnaV@users.noreply.github.com>
2024-02-18 08:10:25 +0000

    reset player transform

--- Assets/Scenes/Level_Height/Scripts/PlayerReset.cs
+++ Assets/Scenes/Level_Height/Scripts/PlayerReset.cs
@@ -1,29 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerReset : MonoBehaviour
 {
-    public GameObject player;
     public Transform resetTransform;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
-        {
-            player.transform.position = resetTransform.position;
+        { 
+            var player = other.gameObject;
+            var controller = player.GetComponent<CharacterController>();
+            if (controller != null)
+            {
+                controller.enabled = false;
+
+                transform.SetPositionAndRotation(resetTransform.position, resetTransform.rotation);
+
+                controller.enabled = true;
+            }
+            else
+            {
+                Debug.LogWarning("Player must have Character controller");
+            }
         }
     }
 }
