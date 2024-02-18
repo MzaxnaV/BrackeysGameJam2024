c47200647075aacab76fc3d61f6b80782eb37abd
Manav <30342951+MzaxnaV@users.noreply.github.com>
2024-02-18 00:46:14 +0000

    fix fps controller

--- Assets/Scripts/RaycastManager.cs
+++ Assets/Scripts/RaycastManager.cs
@@ -1,20 +1,37 @@
+using System;
 using UnityEngine;
 
 public class RaycastManager : MonoBehaviour
 {
+    private MeshRenderer meshRenderer;
+    private static readonly int DoorlsShy = Animator.StringToHash("doorlsShy");
+
+    private int layerMask;
+
+    private void Awake()
+    {
+        layerMask = 1 << LayerMask.NameToLayer("Door");
+    }
+
     void Update() {
         RaycastHit hit;
-        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        
+        // TODO: the issue we have is that forward vector rapidly changes it's values, can you
+        // smooth this over a few frames (1-2 seconds) so it remains stable
+        Vector3 forward = transform.TransformDirection(Vector3.forward).normalized;
+        
         Debug.DrawRay(transform.position, forward, Color.green);
         
-        // Infinity Raycast
-        if (Physics.Raycast(transform.position, forward, out hit, Mathf.Infinity)) {
-            if (hit.collider.CompareTag("Door")) {
-                // Debug.Log("Shy door closed!");
+        if (Physics.Raycast(transform.position, forward, out hit, Mathf.Infinity, layerMask)) {
+            meshRenderer = hit.collider.gameObject.transform.parent.GetComponent<MeshRenderer>();
+            if (meshRenderer != null) {
+                meshRenderer.enabled = true;
             }
-            else
-            {
-                // Debug.Log("Shy door opens!");
+        } else {
+            if (meshRenderer != null) {
+                Debug.Log("Disable MeshRenderer, change this!!!");
+                meshRenderer.enabled = false;
+                meshRenderer = null;
             }
         }
     }
